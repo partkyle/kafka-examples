834aeaea69b1b11cf551b8a53c2928cca5fd2982
Kyle Partridge <partkyle@gmail.com>
2014-07-23 10:09:02 -0700

    added config for partitions to run concurrently

--- consumer/main.go
+++ consumer/main.go
@@ -3,32 +3,75 @@ package main
 import (
 	"flag"
 	"log"
-	"time"
+	"os"
+	"sync"
+
+	"github.com/partkyle/kafka-examples/flagutil"
 
 	"github.com/Shopify/sarama"
 )
 
 var (
-	kafka = flag.String("kafka", ":2181", "kafka addr")
-	topic = flag.String("topic", "default", "topic to post")
+	logger = log.New(os.Stdout, "[kafka-consumer] ", log.LstdFlags)
+
+	kafkas     flagutil.MultiString = flagutil.MultiString{":2181"}
+	topic      string
+	partitions flagutil.MultiInt32
+	offset     int64
 )
 
-func main() {
+func init() {
+	flag.Var(&kafkas, "kafkas", "kafka addresses to connect to")
+	flag.StringVar(&topic, "topic", "default", "topic to read")
+	flag.Var(&partitions, "partitions", "partitions to read from")
+	flag.Int64Var(&offset, "offset", 0, "offset to start reading from")
+
 	flag.Parse()
+}
 
-	clientConfig := &sarama.ClientConfig{MetadataRetries: 10, WaitForElection: 250 * time.Millisecond}
-	client, err := sarama.NewClient("sarama", []string{*kafka}, clientConfig)
+func main() {
+	logger.Printf("using kafka addresses: %v", kafkas)
+	client, err := sarama.NewClient("sarama", []string(kafkas), nil)
 	if err != nil {
-		log.Fatalf("err creating client: %s", err)
+		logger.Fatalf("err creating client: %s", err)
 	}
 
-	consumerConfig := &sarama.ConsumerConfig{MaxWaitTime: 1024}
-	consumer, err := sarama.NewConsumer(client, *topic, 0, "", consumerConfig)
-	if err != nil {
-		log.Fatalf("err creating consumer: %s", err)
+	if partitions == nil {
+		logger.Println("no partitions received - using all partitions")
+
+		var err error
+		partitions, err = client.Partitions(topic)
+		if err != nil {
+			logger.Fatal(err)
+		}
 	}
 
-	for event := range consumer.Events() {
-		log.Printf("%s: %s", event.Key, event.Value)
+	logger.Printf("using partitions %v", partitions)
+
+	var wg sync.WaitGroup
+	wg.Add(len(partitions))
+
+	for _, partition := range partitions {
+		p := partition
+		go func() {
+			defer wg.Done()
+
+			consumerConfig := sarama.NewConsumerConfig()
+			consumerConfig.OffsetMethod = sarama.OffsetMethodManual
+			consumerConfig.OffsetValue = offset
+
+			consumer, err := sarama.NewConsumer(client, topic, p, "", consumerConfig)
+			if err != nil {
+				logger.Fatalf("err creating consumer: %s", err)
+			}
+			defer consumer.Close()
+
+			for event := range consumer.Events() {
+				logger.Printf("partition=%d offset=%d key=%s\n\tvalue=%s", event.Partition, event.Offset, event.Key, event.Value)
+			}
+		}()
 	}
+
+	wg.Wait()
+	logger.Println("exiting normally")
 }
