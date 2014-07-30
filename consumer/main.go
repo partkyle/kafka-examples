d971a5ee1f0f052a6860ed2a0657bad57822951a
Kyle Partridge <partkyle@gmail.com>
2014-07-30 09:56:08 -0700

    added ability to "show-offsets"

--- consumer/main.go
+++ consumer/main.go
@@ -14,17 +14,19 @@ import (
 var (
 	logger = log.New(os.Stdout, "[kafka-consumer] ", log.LstdFlags)
 
-	kafkas     flagutil.MultiString = flagutil.MultiString{":2181"}
-	topic      string
-	partitions flagutil.MultiInt32
-	offset     int64
+	kafkas      flagutil.MultiString = flagutil.MultiString{":2181"}
+	topic       string
+	partitions  flagutil.MultiInt32
+	offset      int64
+	showOffsets bool
 )
 
 func init() {
 	flag.Var(&kafkas, "kafkas", "kafka addresses to connect to")
-	flag.StringVar(&topic, "topic", "default", "topic to read")
+	flag.StringVar(&topic, "topic", "", "topic to read")
 	flag.Var(&partitions, "partitions", "partitions to read from")
 	flag.Int64Var(&offset, "offset", 0, "offset to start reading from")
+	flag.BoolVar(&showOffsets, "show-offsets", false, "show offset and exit")
 
 	flag.Parse()
 }
@@ -37,8 +39,6 @@ func main() {
 	}
 
 	if partitions == nil {
-		logger.Println("no partitions received - using all partitions")
-
 		var err error
 		partitions, err = client.Partitions(topic)
 		if err != nil {
@@ -48,30 +48,54 @@ func main() {
 
 	logger.Printf("using partitions %v", partitions)
 
+	if showOffsets {
+		readOffsets(client, topic, partitions)
+	} else {
+		tailPartitions(client, topic, partitions)
+	}
+}
+
+func readOffsets(client *sarama.Client, topic string, partitions []int32) {
+	for _, partition := range partitions {
+		earliest, err := client.GetOffset(topic, partition, sarama.EarliestOffset)
+		if err != nil {
+			logger.Fatalf("error getting earliest offset for %s:%d: %s", topic, partition, err)
+		}
+
+		latest, err := client.GetOffset(topic, partition, sarama.LatestOffsets)
+		if err != nil {
+			logger.Fatalf("error getting latest offset for %s:%d: %s", topic, partition, err)
+		}
+
+		logger.Printf("topic=%s partition=%d earliest=%d latest=%d", topic, partition, earliest, latest)
+	}
+}
+
+func tailPartitions(client *sarama.Client, topic string, partitions []int32) {
 	var wg sync.WaitGroup
 	wg.Add(len(partitions))
 
+	tailConsumer := func(partition int32) {
+		defer wg.Done()
+
+		consumerConfig := sarama.NewConsumerConfig()
+		consumerConfig.OffsetMethod = sarama.OffsetMethodManual
+		consumerConfig.OffsetValue = offset
+
+		consumer, err := sarama.NewConsumer(client, topic, partition, "", consumerConfig)
+		if err != nil {
+			logger.Fatalf("err creating consumer: %s", err)
+		}
+		defer consumer.Close()
+
+		for event := range consumer.Events() {
+			logger.Printf("partition=%d offset=%d key=%s value=%s", event.Partition, event.Offset, event.Key, event.Value)
+		}
+	}
+
 	for _, partition := range partitions {
-		p := partition
-		go func() {
-			defer wg.Done()
-
-			consumerConfig := sarama.NewConsumerConfig()
-			consumerConfig.OffsetMethod = sarama.OffsetMethodManual
-			consumerConfig.OffsetValue = offset
-
-			consumer, err := sarama.NewConsumer(client, topic, p, "", consumerConfig)
-			if err != nil {
-				logger.Fatalf("err creating consumer: %s", err)
-			}
-			defer consumer.Close()
-
-			for event := range consumer.Events() {
-				logger.Printf("partition=%d offset=%d key=%s\n\tvalue=%s", event.Partition, event.Offset, event.Key, event.Value)
-			}
-		}()
+		go tailConsumer(partition)
 	}
 
 	wg.Wait()
-	logger.Println("exiting normally")
 }
